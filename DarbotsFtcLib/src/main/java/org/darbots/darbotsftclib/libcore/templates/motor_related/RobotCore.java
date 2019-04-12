74c1af2386a84d13502467abdd3e9d752174b26a
Windy Feng <1745500559@qq.com>
2019-04-11 22:25:31 -0400

    Add RobotCore Functionalities

--- DarbotsFtcLib/src/main/java/org/darbots/darbotsftclib/libcore/templates/motor_related/RobotCore.java
+++ DarbotsFtcLib/src/main/java/org/darbots/darbotsftclib/libcore/templates/motor_related/RobotCore.java
@@ -1,8 +1,15 @@
 package org.darbots.darbotsftclib.libcore.templates.motor_related;
 
 import org.darbots.darbotsftclib.libcore.integratedfunctions.RobotLogger;
+import org.darbots.darbotsftclib.libcore.templates.RobotNonBlockingDevice;
 
-public abstract class RobotCore {
+public abstract class RobotCore implements RobotNonBlockingDevice {
     public abstract void stop();
     public abstract RobotLogger getLogger();
+    @Override
+    public void waitUntilFinish(){
+        while(this.isBusy()){
+            this.updateStatus();
+        }
+    }
 }
